91d69653981b1a67b0bd883254e1ccd788c828fc
elvis1056 <john800116@hotmail.com>
2020-07-31 00:08:53 +0800

    Updates

---
+++ precache-manifest.b3912b3758ef97e272e77620f279ac80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4a888330dff76f7e52a7e8106d60a5",
+    "url": "/gomoku/index.html"
+  },
+  {
+    "revision": "93d777686781178b2500",
+    "url": "/gomoku/static/css/main.b166b15d.chunk.css"
+  },
+  {
+    "revision": "f36c00d432ece33975d1",
+    "url": "/gomoku/static/js/2.eb64546d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gomoku/static/js/2.eb64546d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d777686781178b2500",
+    "url": "/gomoku/static/js/main.ebc0b4ca.chunk.js"
+  },
+  {
+    "revision": "5925819172e40a94b9d4",
+    "url": "/gomoku/static/js/runtime-main.fb3ea8af.js"
+  }
+]);
